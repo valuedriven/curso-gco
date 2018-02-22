81e73e962ff4d274a4892996afad6de2a9a09127
concha.yoro <user@conchayoro.com.br>
2018-02-21 22:07:12 -0300

    Revisão das imagens Docker

--- ambiente/tomcat/init_web.sh
+++ ambiente/tomcat/init_web.sh
@@ -4,6 +4,6 @@ cp tomcat-users.xml /usr/local/tomcat/conf
 
 cp  context.xml /usr/local/tomcat/webapps/manager/META-INF/context.xml
 
-cp ./lib/mysql-connector-java-5.1.5.jar /usr/local/tomcat/lib
+cp ./bibliotecas/mysql-connector-java-5.1.5.jar /usr/local/tomcat/lib
 
 
