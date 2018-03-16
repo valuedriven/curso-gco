420fca5a0a7b10329810fff934929849c258f71b
junilson <junilson@hotmail.com>
2018-03-16 07:53:50 -0300

    Configuração de volume para Maven

--- roteiros/roteiro-script-parte0-ambiente.sh
+++ roteiros/roteiro-script-parte0-ambiente.sh
@@ -1,23 +1,19 @@
 #!/bin/sh
 
-echo 'Criando estrutura de diretórios e arquivos ...'
-data_dir=".data"
-jenkins_dir=${data_dir}/jenkins
-nexus_dir=${data_dir}/nexus
-maven_dir=${data_dir}/maven
+#echo 'Criando estrutura de diretórios e arquivos ...'
+#data_dir=".data"
+#jenkins_dir=${data_dir}/jenkins
+#nexus_dir=${data_dir}/nexus
+#maven_dir=${data_dir}/maven
 
-mkdir -p ${nexus_dir}/data
-mkdir -p ${maven_dir}
-mkdir -p ${jenkins_dir}/data
-mkdir -p ${jenkins_dir}/home
+#mkdir -p ${nexus_dir}/data
+#mkdir -p ${maven_dir}
+#mkdir -p ${jenkins_dir}/data
+#mkdir -p ${jenkins_dir}/home
 
-sudo chown -R 200:200 ${nexus_dir}
-sudo chmod g+rwx -R ${maven_dir}
-sudo chmod 777 -R ${jenkins_dir}
-
-echo 'Complementando instalação Maven...'
-
-#cp ambiente/maven/settings.xml ${maven_dir}/settings.xml
+#sudo chown -R 200:200 ${nexus_dir}
+#sudo chmod g+rwx -R ${maven_dir}
+#sudo chmod 777 -R ${jenkins_dir}
 
 echo 'Realizando limpeza no docker...'
 docker container prune -f
@@ -29,9 +25,12 @@ echo 'Removendo todos containers e imagens...'
 #docker rm $(docker ps -a -q) --force
 #docker rmi $(docker images -a -q) --force
 
+echo 'Complementando instalação Maven...'
+#cp ambiente/maven/settings.xml .m2/settings.xml
+
 echo 'Preparando containeres ...'
 docker-compose up -d --build
 
 #echo 'Criando banco de dados ...'
-#docker-compose exec db mysql -u root -psecret conchayorodb < bancoscript/conchayorodb.sql
+docker-compose exec db mysql -u root -psecret conchayorodb < bancoscript/conchayorodb.sql
 
--- roteiros/roteiro-script-parte3-maven.sh
+++ roteiros/roteiro-script-parte3-maven.sh
@@ -7,31 +7,31 @@ echo 'Executando ciclo clean, compile, package, install, deploy...'
 
 echo 'Executando clean...'
 
-docker-compose run --rm maven mvn clean
+docker-compose run --rm maven mvn clean --settings ambiente/maven/settings.xml
 	
 echo 'Executando compile...'
 
-docker-compose run --rm maven mvn compile
+docker-compose run --rm maven mvn compile --settings ambiente/maven/settings.xml
 
 echo 'Executando package...'
 
-docker-compose run --rm maven mvn package
+docker-compose run --rm maven mvn package --settings ambiente/maven/settings.xml
 
 echo 'Executando install...'
 
-docker-compose run --rm maven mvn install
+docker-compose run --rm maven mvn install --settings ambiente/maven/settings.xml
 
 echo 'Executando deploy Nexus...'
 
-docker-compose run --rm maven mvn deploy
+docker-compose run --rm maven mvn deploy --settings ambiente/maven/settings.xml
 
 echo 'Executando deploy Tomcat...'
 
-docker-compose run --rm maven mvn tomcat7:deploy
-docker-compose run --rm maven mvn tomcat7:redeploy
+docker-compose run --rm maven mvn tomcat7:deploy --settings ambiente/maven/settings.xml
+docker-compose run --rm maven mvn tomcat7:redeploy --settings ambiente/maven/settings.xml
 
 echo 'Publicando no Sonar...'
-docker-compose run --rm maven mvn sonar:sonar -Dsonar.host.url=http://sonar:9000
+docker-compose run --rm maven mvn sonar:sonar -Dsonar.host.url=http://sonar:9000 --settings ambiente/maven/settings.xml
 
 
 
