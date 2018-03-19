a4f5a8bfd9e92c18aaddb0e4dba85b5d56bdd176
concha.yorox <userx@conchayoro.com.br>
2018-03-19 16:26:31 -0300

    Retirada do Sonar do serviço

--- roteiros/roteiro-script-parte0-ambiente.sh
+++ roteiros/roteiro-script-parte0-ambiente.sh
@@ -1,36 +1,18 @@
 #!/bin/sh
 
-#echo 'Criando estrutura de diretórios e arquivos ...'
-#data_dir=".data"
-#jenkins_dir=${data_dir}/jenkins
-#nexus_dir=${data_dir}/nexus
-#maven_dir=${data_dir}/maven
-
-#mkdir -p ${nexus_dir}/data
-#mkdir -p ${maven_dir}
-#mkdir -p ${jenkins_dir}/data
-#mkdir -p ${jenkins_dir}/home
-
-#sudo chown -R 200:200 ${nexus_dir}
-#sudo chmod g+rwx -R ${maven_dir}
-#sudo chmod 777 -R ${jenkins_dir}
-
-echo 'Realizando limpeza no docker...'
-docker container prune -f
-docker image prune -f
-docker volume prune -f
-docker network prune -f
+#echo 'Realizando limpeza no docker...'
+#docker container prune -f
+#docker image prune -f
+#docker volume prune -f
+#docker network prune -f
 
 echo 'Removendo todos containers e imagens...'
 #docker rm $(docker ps -a -q) --force
 #docker rmi $(docker images -a -q) --force
 
-echo 'Complementando instalação Maven...'
-#cp ambiente/maven/settings.xml .m2/settings.xml
-
 echo 'Preparando containeres ...'
 docker-compose up -d --build
 
-#echo 'Criando banco de dados ...'
+echo 'Criando banco de dados ...'
 docker-compose exec db mysql -u root -psecret conchayorodb < bancoscript/conchayorodb.sql
 
