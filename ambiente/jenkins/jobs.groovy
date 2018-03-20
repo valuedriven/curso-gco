7ea9a1258c296690a1ce61b90849ef1953dad20b
junilson <junilson@hotmail.com>
2018-03-20 06:25:31 -0300

    Integração Jenkins Docker

--- ambiente/jenkins/jobs.groovy
+++ ambiente/jenkins/jobs.groovy
@@ -2,10 +2,12 @@ folder('conchayoro') {
     description('Estrutura para jobs da ConchayOro')
 }
 
+
 job('conchayoro/compile-package') {
     scm {
         git('http://github.com/valuedriven/curso-gco')
     }
+    
     steps {
         shell('mvn clean --settings ambiente/maven/settings.xml')
         shell('mvn compile --settings ambiente/maven/settings.xml')
@@ -17,6 +19,7 @@ job('conchayoro/install-deploy') {
     scm {
         git('http://github.com/valuedriven/curso-gco')
     }
+
     steps {
         shell('mvn install --settings ambiente/maven/settings.xml')
         shell('mvn deploy --settings ambiente/maven/settings.xml')
@@ -27,6 +30,7 @@ job('conchayoro/deploy-tomcat') {
     scm {
         git('http://github.com/valuedriven/curso-gco')
     }
+
     steps {
         shell('mvn tomcat7:deploy --settings ambiente/maven/settings.xml')
         shell('mvn tomcat7:redeploy --settings ambiente/maven/settings.xml')
