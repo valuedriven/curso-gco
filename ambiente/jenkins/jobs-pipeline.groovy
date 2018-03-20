34c8b1c78096a51a79145f6a82e99322620fe6f2
junilson <junilson@hotmail.com>
2018-03-20 04:33:49 -0300

    Integração Jenkins Docker

--- ambiente/jenkins/jobs-pipeline.groovy
+++ ambiente/jenkins/jobs-pipeline.groovy
@@ -1,26 +1,56 @@
 pipelineJob('conchayoro-pipeline') {
+
   definition {
+
     cpsScm {
+
       scm {
+
         git {
+
           remote {
+
             credentials('conchayoro')
+
             url('http://github.com/valuedriven/curso-gco')
+
           }
+
         }
+
       }
+
       scriptPath('Jenkinsfile')
+
     }
+
   }
+
   scm {
+
     git {
+
       remote {
+
         credentials('conchayoro')
-            url('http://github.com/valuedriven/curso-gco')
+
+        url('http://github.com/valuedriven/curso-gco')
+
       }
+
     }
+
   }
+
   triggers {
+
       cron('@midnight')
+
+  }
+
+  configure {
+
+     it / definition / lightweight(true)
+
   }
 }
